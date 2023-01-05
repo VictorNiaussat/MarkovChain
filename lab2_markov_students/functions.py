c05207461b0d05a6ede6650abac9a8cbdaedd9c0
Victor C <victor.cilleros.pro@gmail.com>
2023-01-05 15:11:33 +0100

    Exo 3 au propre et documentation

--- lab2_markov_students/functions.py
+++ lab2_markov_students/functions.py
@@ -106,6 +106,7 @@ def draw_queue(X,T):
 
 
 def plotHistogramm(X,weights,rho,alpha=0.2,step_numbers = 1000,affiche_step=False):
+    plt.style.use('ggplot')
     valeurs_prises = np.unique(X)
     plt.hist(X,bins=valeurs_prises,density=True,weights=weights,label='Valeurs simulées',color='b')
     plt.bar(valeurs_prises,stationary_distrib_mm1inf(valeurs_prises,rho),color='r',alpha=alpha,label='Valeurs théoriques')
@@ -119,6 +120,7 @@ def plotHistogramm(X,weights,rho,alpha=0.2,step_numbers = 1000,affiche_step=Fals
     plt.show()
 
 def plotHistogrammK(X,weights,rho,K,alpha=0.2,step_numbers = 1000,affiche_step=False):
+    plt.style.use('ggplot')
     valeurs_prises_mm1K= np.unique(X)
     a,bins, _= plt.hist(X[:-1],bins=range(K+2),density=True,weights=np.diff(weights),label='Valeurs simulées',color='b')
     plt.bar(valeurs_prises_mm1K+0.5,stationary_distrib_mm1K(valeurs_prises_mm1K,rho,K),alpha=alpha,color='r',label='Valeurs théoriques')
@@ -175,3 +177,123 @@ def metro_hasting_ising_2D(N,beta,num_samples,display_iter):
 #***********************************************************************************************************************
 #                                               EXERCICE 3
 #***********************************************************************************************************************
+
+
+def draw_neighbour(xn):
+    """
+    :param xn(float):paramètre considéré
+    :return: un voisin du paramètre à +- 0.005 près selon une loi uniforme
+    """
+    return(xn+0.005 if np.random.uniform()<0.5 else xn-0.005 )
+def acceptance_probability(f,y,xn,Tn):
+    """
+    :param f(lambda): fonction considérée
+    :param y: voisin de xn
+    :param xn: particule considérée
+    :param Tn: Température pour la probabilité d'acceptation
+    :return: (float) dans [0,1], probabilité d'acceptation
+    """
+    return(1 if f(y)<f(xn) else np.exp(-(f(y)-f(xn))/Tn))
+
+def simulated_annealing(f,N,T_min,x0,T0):
+    """
+    :param f(lambda): fonction considérée
+    :param N: nombre d'itérations avant la fin de l'algorithme
+    :param T_min: précision maximale
+    :param x0: particule de départ
+    :param T0: température initiale pour la probabilité d'acceptance
+    :return: tuple (X,F) contenant deux array respectivempent : évolution de x, évolution de f(x)
+    """
+    n = 0
+    xn = x0
+    Tn=T0
+    X,F=[],[]
+
+    while n<N and Tn>T_min:
+        y = draw_neighbour(xn)
+        u = np.random.uniform()
+        xn = y if u <= acceptance_probability(f,y,xn,Tn) else xn
+        X.append(xn)
+        F.append(f(xn))
+        Tn = T0/np.log(n+2)
+        n=n+1
+    return(X,F)
+
+def plotAnnealing(X,step_number=1000,drawSteps = False):
+    """
+    :param X (array-like): évolution du paramètre x
+    :param step_number (int): nombre de steps considérés
+    :param drawSteps (Boolean): affichage ou non du nombre d'étapes
+    :return: None, affiche le graphique de l'avolution de X
+    """
+    plt.style.use('ggplot')
+    plt.plot(X)
+    st="Convergence du minimum x recherché"
+    if drawSteps:
+        st=st+f" - Nombre d'itérations : {step_number}"
+    plt.title(st)
+    plt.xlabel("itérations")
+    plt.ylabel("Valeur de x")
+    plt.show()
+
+
+
+def create_city(K,max_size_city,etalement):
+    """
+    :param K(int): nombre de villes à tirer
+    :param max_size_city(int): valeur maximale des coordonnées
+    :param etalement (int): variance considérée
+    :return: (array-like) une grille contenant les coordonnées des villes sur un plan 2D
+    """
+    grid=[]
+    while len(grid)<K:
+        grid.append(np.random.randint(max_size_city, size=2)+np.random.normal(scale = etalement/10,size = 2))
+    grid = np.array(grid)
+    return grid
+
+def distance_permut(sigma,dist,grid):
+    """
+    :param sigma(array-like): permutation considérée
+    :param dist(lambda): calcule une norme entre deux vecteurs (ici distance)
+    :param grid(array-like): grille considérée des villes par leur coordonnées
+    :return: (int) coût total de cette permutation
+    """
+    K=len(sigma)
+    return np.sum([dist(grid[sigma[j]], grid[sigma[(j + 1) % K]]) for j in range(len(grid))])
+
+def generate_indices(K):
+    """
+    :param K(int): indice maximal à considérer
+    :return: tuple(int,int) deux indices différents parmis [0,K]
+    """
+    i = np.random.randint(0, K)
+    k = i
+    while k == i:
+        k = np.random.randint(0, K)
+    return min(i,k), max(i,k)
+
+def draw_neighbourTSP(sigman):
+    """
+    :param sigman(array-like): tableau des permutations condidérées
+    :return: (array-like) permutation aléatoire de deux éléments différents
+    """
+    sigmanp1 = np.copy(sigman)
+    i,k = generate_indices(len(sigman))
+    sigmanp1[i:k+1] = np.flip(sigmanp1[i:k+1])
+    return(sigmanp1)
+
+
+def plotTSP(grid):
+    """
+    :param grid(array-like): grille considérée des villes par leur coordonnées
+    :return: None,affiche le graphe de l'évolution
+    """
+    plt.style.use('ggplot')
+    plt.grid()
+    plt.scatter(grid[:,0],grid[:,1],marker = "P",color = "red")
+    plt.title("Villes du problème considéré")
+    plt.xlabel("coordonée x")
+    plt.ylabel("coordonnée y")
+    plt.show()
+
+
